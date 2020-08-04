bb00da11dc83427f516686dae81c57bbbadefbe5
wengyeowyeap <aquamagmayeow94@gmail.com>
2020-08-04 15:58:30 +0800

    Signup validation: password

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,5 +1,6 @@
 import os
 import app
+import re
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from models.user import User
 from werkzeug.security import generate_password_hash
@@ -20,17 +21,20 @@ def create():
     username = request.form.get('username')
     email = request.form.get('email')
     password = request.form.get('password')
-    hashed_password = generate_password_hash(password) # store this in database
-    
-    new_user = User(username = username, password = hashed_password, email = email)
 
-    if new_user.save():
-        flash("Successfully signed up!")
-        return redirect(url_for('users.show', username=username))
-    else:
-        for error in new_user.errors:
-            flash(error)
+    if (len(password) < 6) or (re.search('[A-Z]',password) is None) or (re.search('[a-z]',password) is None) or (re.search('[0-9]',password) is None) or (re.search('[!@#$%]',password) is None):
+        flash('Password requirement: 6 or more characters, uppercase letters, lowercase letters, numbers, special characters(!@#$%).')
         return render_template('users/new.html')
+    else:
+        hashed_password = generate_password_hash(password) # store this in database    
+        new_user = User(username = username, password = hashed_password, email = email)
+        if new_user.save():
+            flash("Successfully signed up!")
+            return redirect(url_for('users.show', username=username))
+        else:
+            for error in new_user.errors:
+                flash(error)
+            return render_template('users/new.html')
 
 @users_blueprint.route('/<username>', methods=["GET"])
 def show(username):
