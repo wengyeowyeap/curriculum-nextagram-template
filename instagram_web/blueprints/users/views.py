e5e58657a80d229bdfd1f648f02b26125288842a
wengyeowyeap <aquamagmayeow94@gmail.com>
2020-08-05 10:20:34 +0800

    moved pw validation from views to model

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,8 +1,6 @@
 import os
-import re
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from models.user import User
-from werkzeug.security import generate_password_hash
 
 users_blueprint = Blueprint('users',
                             __name__,
@@ -20,19 +18,15 @@ def create():
     email = request.form.get('email')
     password = request.form.get('password')
 
-    if (len(password) < 6) or (re.search('[A-Z]',password) is None) or (re.search('[a-z]',password) is None) or (re.search('[0-9]',password) is None) or (re.search('[!@#$%]',password) is None):
-        flash('Password requirement: 6 or more characters, uppercase letters, lowercase letters, numbers, special characters(!@#$%).')
-        return render_template('users/new.html')
+    new_user = User(username = username, password = password, email = email)
+
+    if new_user.save():
+        flash("Successfully signed up!")
+        return redirect(url_for('users.show', username=username))
     else:
-        hashed_password = generate_password_hash(password) # store this in database    
-        new_user = User(username = username, password = hashed_password, email = email)
-        if new_user.save():
-            flash("Successfully signed up!")
-            return redirect(url_for('users.show', username=username))
-        else:
-            for error in new_user.errors:
-                flash(error)
-            return render_template('users/new.html')
+        for error in new_user.errors:
+            flash(error)
+        return redirect(url_for('users.new'))
 
 @users_blueprint.route('/<username>', methods=["GET"])
 def show(username):
--- models/user.py
+++ models/user.py
@@ -1,10 +1,13 @@
 from models.base_model import BaseModel
 import peewee as pw
+import re
+from werkzeug.security import generate_password_hash
 
 class User(BaseModel):
-    username = pw.CharField(unique=True)
-    password = pw.CharField()
-    email = pw.CharField(unique=True)
+    username = pw.CharField(unique=True, null=False)
+    email = pw.CharField(unique=True, null=False)
+    password_hash = pw.TextField(null=False)
+    password = None
 
     def validate(self):
         username_duplicate = User.get_or_none(User.username == self.username)
@@ -16,3 +19,9 @@ class User(BaseModel):
             self.errors.append('Username has been used')
         elif email_duplicate:
             self.errors.append('Email has been used')
+        
+        if (len(self.password) < 6) or (re.search('[A-Z]',self.password) is None) or (re.search('[a-z]',self.password) is None) or (re.search('[0-9]',self.password) is None) or (re.search('[!@#$%]',self.password) is None):
+            self.errors.append('Password requirement: 6 or more characters, uppercase letters, lowercase letters, numbers, special characters(!@#$%).')
+        else:
+            self.password_hash = generate_password_hash(self.password) # store this in database    
+        
