b83834ce21342fb57b267a8e14f82c015bfbae57
wengyeowyeap <aquamagmayeow94@gmail.com>
2020-08-06 16:00:34 +0800

    setup aws s3 environment

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 import os
 
-
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = os.environ.get(
-        'SECRET_KEY') or os.urandom(32)
-
+    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(32)
+    S3_BUCKET   = os.environ.get("S3_BUCKET_NAME")
+    S3_KEY      = os.environ.get("S3_ACCESS_KEY")
+    S3_SECRET   = os.environ.get("S3_SECRET_ACCESS_KEY")
+    S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
 
 class ProductionConfig(Config):
     DEBUG = False
